d903dabe2744cc20ce3af2fda2aeba680dd21537
Kiara Bici <kiara.bici16@gmail.com>
2024-04-19 18:25:36 +0200

    firstcommit

---
+++ src/main/java/com/hendisantika/springbootrestapipostgresql/entity/Author.java
@@ -0,0 +1,71 @@
+package com.hendisantika.springbootrestapipostgresql.entity;
+
+import jakarta.persistence.CollectionTable;
+import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+    private String name;
+    private String surname;
+    private String isbn;
+
+    public Author(String name, String surname, String isbn) {
+        this.name = name;
+        this.surname = surname;
+        this.isbn = isbn;
+    }
+
+    public Author() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    @Override
+    public String toString() {
+        return "Author{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", isbn='" + isbn + '\'' +
+                '}';
+    }
+}
